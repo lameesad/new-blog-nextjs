f09a26497980d5fd544c567f321f87cc54a54d31
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-15 17:39:13 +0300

    Rendering Markdown As JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from 'react-markdown';
+
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 
@@ -9,13 +11,13 @@ const DUMMY_POST = {
   content: '# This is a first post',
 };
 
-function PostContent(props) {
+function PostContent() {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
 
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
