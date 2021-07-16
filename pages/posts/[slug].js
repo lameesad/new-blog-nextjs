1586b1e2216a483385355b86099d398b72e4e883
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-16 15:58:52 +0300

    Rendering Dynamic Post Pages & Paths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,32 +1,32 @@
 import PostContent from "../../components/posts/post-detail/post-content"
-import { getAllPosts, getPostData, getPostsFiles } from "../../lib/posts-util"
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
 function PostDetailPage(props) {
-    return <PostContent post={props.post} />
+    return <PostContent post={props.post} />;
 }
 
-export function getStativProps(context) {
-    // context contain the value slug in the url
-    const { params } = context
-    const { slug } = params
+export function getStaticProps(context) {
+    const { params } = context;
+    const { slug } = params;
 
     const postData = getPostData(slug);
 
     return {
         props: {
-            post: postData
+            post: postData,
         },
-        revalidate: 600
-    }
+        revalidate: 600,
+    };
 }
 
 export function getStaticPaths() {
-    const postFilenames = getPostsFiles()
+    const postFilenames = getPostsFiles();
 
-    const slugs = postFiles.map(fileName => fileName.replace(/\.md$/, ''));
+    const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
     return {
-        paths: slugs.map(slug => ({ params: { slug: slug } })),
-        fallback: false
-    }
+        paths: slugs.map((slug) => ({ params: { slug: slug } })),
+        fallback: false,
+    };
 }
 export default PostDetailPage
