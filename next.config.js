19ad7ada0c45da7c40b93863be5a5bf93591c6f7
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-17 18:19:58 +0300

    The NextJS Config File & Working With Environment Variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  env: {
+    mongodb_username: 'userdb',
+    mongodb_password: 'userdb',
+    mongodb_clustername: 'cluster0',
+    mongodb_database: 'my-site'
+  },
+
   reactStrictMode: true,
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,10 +24,9 @@ async function handler(req, res) {
 
         let client;
 
-
-
+        const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.bp9sj.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
         try {
-            client = await MongoClient.connect('mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+            client = await MongoClient.connect(connectionString);
         } catch (error) {
             res.status(500).json({ message: 'Could not connect to database.' });
             return;
