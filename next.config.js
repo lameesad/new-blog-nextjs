3b3bf32d80beac80fcee1ce653fd83605ea1b5de
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-17 18:29:00 +0300

    optimizing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
-module.exports = {
-  env: {
-    mongodb_username: 'userdb',
-    mongodb_password: 'userdb',
-    mongodb_clustername: 'cluster0',
-    mongodb_database: 'my-site'
-  },
 
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'userdb',
+        mongodb_password: 'userdb',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site-dev'
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'userdb',
+      mongodb_password: 'userdb',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-site'
+    },
+  };
+};
