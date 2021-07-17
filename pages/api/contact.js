c22cdf0b75af7fefddc1e53b24c9fe4bb4d1f9eb
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-07-17 15:18:14 +0300

    Adding UI Feedback With Notifications

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,10 +24,10 @@ async function handler(req, res) {
 
         let client;
 
+
+
         try {
-            client = await MongoClient.connect(
-                'mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/my-site?retryWrites=true&w=majority'
-            );
+            client = await MongoClient.connect('mongodb+srv://userdb:userdb@cluster0.bp9sj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
         } catch (error) {
             res.status(500).json({ message: 'Could not connect to database.' });
             return;
